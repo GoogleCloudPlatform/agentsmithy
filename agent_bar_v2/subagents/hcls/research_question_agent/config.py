02db7e5ab5cdba34302b357b99d27e96638bbb09
Ahmad Khan <akhanduke@gmail.com>
2026-03-05 06:10:42 -0500

    refactored research_question_agent to gold standard

--- agent_bar_v2/subagents/hcls/research_question_agent/config.py
+++ agent_bar_v2/subagents/hcls/research_question_agent/config.py
@@ -12,6 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .agent import root_agent
-
-__all__ = ["agent"]
+"""Configuration for the research_question_agent."""
--- agent_bar_v2/subagents/hcls/research_question_agent/prompts.py
+++ agent_bar_v2/subagents/hcls/research_question_agent/prompts.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 """Prompt for the root agent."""
 
-RQ_PROMPT = """
+SYSTEM_INSTRUCTION = """
 You are a highly skilled biomedical researcher specializing in literature review and scientific synthesis.
 You have two sub agents to help you search Pubmed and generate hypotheses
 
