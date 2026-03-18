558eed0d213d3945e3e6288b07f061036aba7a6a
niveabekal <niveabekal@google.com>
2026-03-18 14:43:58 -0500

    Update config.py

--- agent_bar_v2/subagents/retail/customer_support_hub/config.py
+++ agent_bar_v2/subagents/retail/customer_support_hub/config.py
@@ -17,10 +17,10 @@
 import os
 
 # Google Cloud Configuration
-PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT") #GOOGLE_CLOUD_PROJECT=ai-agent-bar-2026-stage
-LOCATION = os.environ.get("GOOGLE_CLOUD_LOCATION") #GOOGLE_CLOUD_LOCATION=us-central1
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT") # GOOGLE_CLOUD_PROJECT=ai-agent-bar-2026-stage
+LOCATION = os.environ.get("GOOGLE_CLOUD_LOCATION") # GOOGLE_CLOUD_LOCATION=us-central1
 
-#GOOGLE_GENAI_USE_VERTEXAI=true
+# GOOGLE_GENAI_USE_VERTEXAI=true
 
 # Vertex AI Search
 DATASTORE_ID=retail-site-search_1773188929137
