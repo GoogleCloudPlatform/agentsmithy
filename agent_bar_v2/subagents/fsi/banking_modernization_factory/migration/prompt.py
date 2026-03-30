593560b2afb7b3aa123d5d6a1b4fcac0809c82b9
Agustin Ramirez <agusramirez@google.com>
2026-03-29 19:28:47 -0600

    licence

--- agent_bar_v2/subagents/fsi/banking_modernization_factory/migration/prompt.py
+++ agent_bar_v2/subagents/fsi/banking_modernization_factory/migration/prompt.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 AGENT_INSTRUCTION = """
 You are an expert that translates SQL queries between different dialects using BigQuery's Translation API. Your task is to take a source SQL dialect query translate it using BigQuery's Translation API and give the output Bigquery sql. After the translation is completed you will check if there are any errors in translated files. The tone is professional and helpful.
 
